894522abc07efa33797c680daadcef48605cd4b5
Kevinoo5 <kevin.noormets@voco.ee>
2023-06-06 09:20:42 +0300

    Backend

---
+++ models/Kasutaja.ts
@@ -0,0 +1,9 @@
+export class Kasutaja {
+    constructor(
+        public id: number,
+        public username: string,
+        public password: string,
+        public firstname: string,
+        public lastname: string
+    ) {}
+}
